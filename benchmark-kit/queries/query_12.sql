bb9ef7e964349ddcd5a373837d11bc0d58bc5b07
Nathan Meinzer <nathan.meinzer@dremio.com>
2025-07-15 02:31:52 -1000

    commit and push deploying reflections

--- benchmark-kit/queries/query_12.sql
+++ benchmark-kit/queries/query_12.sql
@@ -12,11 +12,4 @@ group by sr_customer_sk
 from customer_total_return ctr1
 ,store
 ,customer
-where ctr1.ctr_total_return > (select avg(ctr_total_return)*1.2
-from customer_total_return ctr2
-where ctr1.ctr_store_sk = ctr2.ctr_store_sk)
-and s_store_sk = ctr1.ctr_store_sk
-and s_state = 'TN'
-and ctr1.ctr_customer_sk = c_customer_sk
-order by c_customer_id
 limit 100;
